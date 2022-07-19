bf230d202cd860ddfb096fbf5b61fbb0de8c61f3
Fransiscus Kevin <kevinsuryaperdana@gmail.com>
2022-07-19 15:01:34 +0700

    slider

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -1,6 +1,30 @@
 <?= $this->extend('layout/template'); ?>
 
 <?= $this->section('content'); ?>
+<div class="container-fluid">
+      <div id="carouselSlide" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="/public/img/slider1.jpg" class="d-block w-100" alt="Gambar 1" />
+          </div>
+          <div class="carousel-item">
+            <img src="/public/img/slider2.jpg" class="d-block w-100" alt="Gambar 2" />
+          </div>
+          <div class="carousel-item">
+            <img src="/public/img/slider3.jpg" class="d-block w-100" alt="Gambar 3" />
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselSlide" data-bs-slide="prev">
+          <i class="fa fa-chevron-left" aria-hidden="true"></i>
+          <span class="sr-only">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselSlide" data-bs-slide="next">
+          <i class="fa fa-chevron-right" aria-hidden="true"></i>
+          <span class="sr-only">Next</span>
+        </button>
+      </div>
+         </div>
+
 <section class="showcase">
       <div class="container-fluid p-0">
         <div class="row g-0">
