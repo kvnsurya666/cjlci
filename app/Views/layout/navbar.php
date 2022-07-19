6bed059791a87929cb88a1c7d47cfa9f9e2c34e9
Fransiscus Kevin <kevinsuryaperdana@gmail.com>
2022-07-19 14:48:10 +0700

    ubah navbar

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -1,29 +1,31 @@
-<nav class="navbar navbar-expand-lg bg-light sticky-top">
-  <div class="container">
-    <a class="navbar-brand" href="#">
-    <img src="/public/img/logo ciptajayalestari.PNG" alt="" width="100" height="50" />
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav justify-content-center">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Beranda</a>
-        </li>
-        <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Katalog</a>
+<nav class="navbar-dark sticky-top bg-light">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-1">
+            <a class="navbar-brand" href="#" style="padding-top: 10px; padding-bottom: 10px">
+              <img src="/public/img/logo ciptajayalestari.PNG" alt="" width="100" height="50" />
+            </a>
+          </div>
+
+          <div class="col-md-11" id="navbarNav">
+            <ul class="nav justify-content-center">
+              <li class="nav-item">
+                <a class="nav-link active" href="#">Home</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="/views/katalog/produk.php" role="button" aria-expanded="false">Katalog</a>
                 <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="/views/katalog/mesin.php">Mesin</a></li>
+                  <li><a class="dropdown-item" href="pages/katalog">Mesin</a></li>
                   <li><a class="dropdown-item" href="/views/katalog/oli.php">Oli</a></li>
                   <li><a class="dropdown-item" href="/views/katalog/aksesoris.php">Aksesoris</a></li>
                   <li><a class="dropdown-item" href="/views/katalog/jasa.php">Jasa</a></li>
                 </ul>
               </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Tentang Kami</a>
-        </li>      
-      </ul>
-    </div>
-  </div>
-</nav>
+              <li class="nav-item">
+                <a class="nav-link" href="/views/about/tentangkami.php">Tentang Kami</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
