0e542ccfab331d0fc6fb029a4b284e9d5ac7d340
Fransiscus Kevin <kevinsuryaperdana@gmail.com>
2022-07-19 17:10:01 +0700

    revisi routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -37,6 +37,7 @@ $routes->set404Override();
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
 $routes->get('/users', 'Admin\Users::index');
+$routes->get('/pages', 'Pages::tentangkami');
 
 /*
  * --------------------------------------------------------------------
