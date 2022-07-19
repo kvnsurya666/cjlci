3626c9ad790315700d5b1d9b452b45639c06c1de
Fransiscus Kevin <kevinsuryaperdana@gmail.com>
2022-07-19 14:21:03 +0700

    controller admin

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -35,7 +35,8 @@ $routes->set404Override();
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::home');
+$routes->get('/', 'Home::index');
+$routes->get('/users', 'Admin\Users::index');
 
 /*
  * --------------------------------------------------------------------
---
+++ app/Controllers/Admin/Users.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers\Admin;
+
+use App\Controllers\BaseController;
+
+class Users extends BaseController
+{
+    public function index()
+    {
+		echo 'Halo, Selamat datang di halaman Admin';
+    }
+}
