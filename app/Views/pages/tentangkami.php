c4e2b7300e2d9403c1ae84204600cbfba1cbf09b
Fransiscus Kevin <kevinsuryaperdana@gmail.com>
2022-07-19 12:46:24 +0700

    tentang kami

---
+++ app/Views/pages/tentangkami.php
@@ -0,0 +1,13 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>Tentang Kami</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore, est!</p>
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?>
