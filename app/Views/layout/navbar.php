6c4c8560142ac617bbe1ea3d60251cc48118fe34
Fransiscus Kevin <kevinsuryaperdana@gmail.com>
2022-07-20 12:56:18 +0700

    testing url

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -15,14 +15,14 @@
               <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Katalog</a>
                 <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="<?= base_url('/pages/mesin'); ?>">Mesin</a></li>
-                  <li><a class="dropdown-item" href="<?= base_url('/pages/oli'); ?>">Oli</a></li>
-                  <li><a class="dropdown-item" href="<?= base_url('/pages/aksesoris'); ?>">Aksesoris</a></li>
-                  <li><a class="dropdown-item" href="<?= base_url('/pages/jasa'); ?>">Jasa</a></li>
+                  <li><a class="dropdown-item" href="<?= base_url('/mesin'); ?>">Mesin</a></li>
+                  <li><a class="dropdown-item" href="<?= base_url('/oli'); ?>">Oli</a></li>
+                  <li><a class="dropdown-item" href="<?= base_url('/aksesoris'); ?>">Aksesoris</a></li>
+                  <li><a class="dropdown-item" href="<?= base_url('/jasa'); ?>">Jasa</a></li>
                 </ul>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="<?= base_url('/pages/tentangkami'); ?>">Tentang Kami</a>
+                <a class="nav-link" href="<?= base_url('/tentangkami'); ?>">Tentang Kami</a>
               </li>
             </ul>
           </div>
